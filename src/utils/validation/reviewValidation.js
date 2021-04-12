5d8cc9de38a47983f378833c33b9b63b0c176e7c
margu31 <judypark31@gmail.com>
2021-04-12 17:07:54 +0900

    [기능] review 작성 validation 코드 수정

--- src/utils/validation/reviewValidation.js
+++ src/utils/validation/reviewValidation.js
@@ -1,4 +1,4 @@
 // 모든 글자 1글자 이상 500글자 이하
 export const reviewRegExp = /^.{1,500}$/;
 
-export const isVaildReviewFormat = (value) => reviewRegExp.test(value);
+export const reviewValidation = (value) => reviewRegExp.test(value);
