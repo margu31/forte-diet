5d9b86f6ab5a8ffcebb814254ebe98135ab1cfcf
lhhyung91 <lhhyung91@gmail.com>
2021-04-12 21:14:29 +0900

    [components] Button components 제작

---
+++ src/components/Button/Button.styled.js
@@ -0,0 +1,11 @@
+import styled, { css } from "styled-components";
+
+export const StyledButton = styled.button`
+  ${({ width, height }) => css`
+    width: ${width}px;
+    height: ${height}px;
+  `}
+  border: 0;
+  background-color: transparent;
+  cursor: pointer;
+`;
