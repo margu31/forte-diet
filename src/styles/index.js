a05f9cb2408df09ca25487adb40112d510ec492f
hyorard-b <reddream08@gmail.com>
2021-04-13 17:59:56 +0900

    [스타일] theme palette 추가 및 reset 스타일 적용

--- src/styles/index.js
+++ src/styles/index.js
@@ -1,8 +1,37 @@
 import { createGlobalStyle } from 'styled-components';
 
 const GlobalStyle = createGlobalStyle`
-  body {
-    margin: 0;
+html, body, div, span, applet, object, iframe,
+h1, h2, h3, h4, h5, h6, p, blockquote, pre,
+a, abbr, acronym, address, big, cite, code,
+del, dfn, em, img, ins, kbd, q, s, samp,
+small, strike, strong, sub, sup, tt, var,
+b, u, i, center,
+dl, dt, dd, ol, ul, li,
+fieldset, form, label, legend,
+table, caption, tbody, tfoot, thead, tr, th, td,
+article, aside, canvas, details, embed, 
+figure, figcaption, footer, header, hgroup, 
+menu, nav, output, ruby, section, summary,
+time, mark, audio, video {
+	margin: 0;
+	padding: 0;
+	border: 0;
+	font-size: 100%;
+	font: inherit;
+	vertical-align: baseline;
+  text-decoration: none;
+  box-sizing: border-box;
+}
+/* HTML5 display-role reset for older browsers */
+article, aside, details, figcaption, figure, 
+footer, header, hgroup, menu, nav, section {
+	display: block;
+  box-sizing: border-box;
+}
+body {
+	line-height: 1;
+  margin: 0;
     padding: 0;
     background: #F4F3EF;
     font-family: Chungchunsidae, sans-serif, -apple-system, BlinkMacSystemFont,
@@ -10,7 +39,34 @@ const GlobalStyle = createGlobalStyle`
     'Droid Sans', 'Helvetica Neue';
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  }
+}
+ol, ul {
+	list-style: none;
+}
+blockquote, q {
+	quotes: none;
+}
+blockquote:before, blockquote:after,
+q:before, q:after {
+	content: '';
+	content: none;
+}
+table {
+	border-collapse: collapse;
+	border-spacing: 0;
+}
 `;
+
+export const palette = {
+  themePrimary: '#F3B34C',
+  themeSecondary: '#395B9D',
+  themeBright: '#F4F3EF', // 배경 베이지색
+  themeBrightGray: '#B6B6B6',
+  themeDark: '#626262',
+  themeDefault: '#14182C',
+  themeDisable: '#878787',
+  themeHover: '#F5DEB7',
+  themeDefaultWhite: '#FFF'
+};
 
 export default GlobalStyle;
