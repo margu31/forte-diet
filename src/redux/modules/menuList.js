6815a57b5eb42ff7851f757da14662d20a753c50
jjhstoday <jjh.s.today@gmail.com>
2021-04-13 21:22:34 +0900

    [상태 관리] daily review 상태 리덕스 생성

--- src/redux/modules/menuList.js
+++ src/redux/modules/menuList.js
@@ -2,6 +2,9 @@ const GET_MENU_LIST = 'mypage/GET_MENU_LIST';
 const DELETE_MENU_LIST = 'mypage/DELETE_MENU_LIST';
 const ADD_WATER_DOSE = 'mypage/ADD_WATER_DOSE';
 const RESET_WATER_DOSE = 'mypage/RESET_WATER_DOSE';
+const ADD_DAILY_REVIEW = 'mypage/ADD_DAILY_REVIEW';
+const EDIT_DAILY_REVIEW = 'mypage/ADD_DAILY_REVIEW';
+const DELETE_DAILY_REVIEW = 'mypage/ADD_DAILY_REVIEW';
 
 export const getMenuListAction = data => ({
   type: GET_MENU_LIST,
@@ -29,6 +32,14 @@ export const getMenuListAction = data => ({
 //   }
 // });
 
+export const addDailyReview = (date, review) => ({
+  type: ADD_DAILY_REVIEW,
+  payload: {
+    date,
+    review
+  }
+});
+
 const initialState = {};
 
 export default function menuList(state = initialState, action) {
@@ -43,6 +54,14 @@ export default function menuList(state = initialState, action) {
     //   return state;
     // case RESET_WATER_DOSE:
     //   return state;
+    case ADD_DAILY_REVIEW:
+      return {
+        ...state,
+        [action.date]: {
+          ...state[action.date],
+          dailyReview: action.review
+        }
+      };
     default:
       return state;
   }
