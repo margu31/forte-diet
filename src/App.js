65d235fa743dd555a2b595d1fc46742c7da135b2
hyorard-b <reddream08@gmail.com>
2021-04-13 10:40:16 +0900

    [빌드] jsconfig.json 추가 및 적용

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import LogInContainer from './containers/LogInContainer/LogInContainer';
+import LogInContainer from 'containers/LogInContainer/LogInContainer';
 import GlobalStyle from './styles';
-import MyPage from './pages/MyPage/MyPage';
+import MyPage from 'pages/MyPage/MyPage';
 import './App.css';
 
 function App() {
--- src/containers/PostingContainer/PostingContainer.js
+++ src/containers/PostingContainer/PostingContainer.js
@@ -1,17 +1,17 @@
-import { useState } from "react";
-import { useDispatch } from "react-redux";
-import PostingButton from "../../components/Posting/PostingButton";
-import PostingForm from "../../components/Posting/PostingForm";
-import PostingInput from "../../components/Posting/PostingInput";
-import PostingInputRadio from "../../components/Posting/PostingInputRadio";
-import PostingReviewBox from "../../components/Posting/PostingReviewBox";
-import { addMealAction } from "../../redux/modules/postingMenuList";
-import { reviewValidation } from "../../utils/validation/reviewValidation";
+import { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import PostingButton from 'components/Posting/PostingButton';
+import PostingForm from 'components/Posting/PostingForm';
+import PostingInput from 'components/Posting/PostingInput';
+import PostingInputRadio from 'components/Posting/PostingInputRadio';
+import PostingReviewBox from 'components/Posting/PostingReviewBox';
+import { addMealAction } from 'redux/modules/postingMenuList';
+import { reviewValidation } from 'utils/validation/reviewValidation';
 
 const initialPostingFormValues = {
   photo: null,
   calories: 0,
-  review: "",
+  review: '',
   isPublic: null,
   type: null,
   hasError: {
@@ -19,22 +19,22 @@ const initialPostingFormValues = {
     calories: null,
     review: null,
     isPublic: null,
-    type: null,
-  },
+    type: null
+  }
 };
 
 function PostingContainer() {
   const dispatch = useDispatch();
   const [mealData, setMealData] = useState(initialPostingFormValues);
 
-  const onChange = (e) => {
+  const onChange = e => {
     setMealData({
       ...mealData,
-      [e.target.name]: e.target.value,
+      [e.target.name]: e.target.value
     });
   };
 
-  const onSubmit = (e) => {
+  const onSubmit = e => {
     e.preventDefault();
 
     const formData = new FormData();
@@ -48,23 +48,23 @@ function PostingContainer() {
     dispatch(addMealAction(newFormData));
   };
 
-  const onBlur = (e) => {
+  const onBlur = e => {
     const validation = reviewValidation(e.target.value);
     if (!validation) {
       setMealData({
         ...mealData,
         hasError: {
           ...mealData.hasError,
-          review: "한 글자 이상 입력해주세요!",
-        },
+          review: '한 글자 이상 입력해주세요!'
+        }
       });
     } else {
       setMealData({
         ...mealData,
         hasError: {
           ...mealData.hasError,
-          review: null,
-        },
+          review: null
+        }
       });
     }
   };
@@ -72,34 +72,34 @@ function PostingContainer() {
   return (
     <PostingForm>
       <PostingInput
-        type="date"
-        id="postingDate"
-        name="postingDate"
-        label="DATE"
+        type='date'
+        id='postingDate'
+        name='postingDate'
+        label='DATE'
         onChange={onChange}
         required
       />
       <PostingInput
-        type="file"
-        id="postingFile"
-        name="postingFile"
-        label="PHOTO"
-        placeholder="사진을 선택하세요!"
+        type='file'
+        id='postingFile'
+        name='postingFile'
+        label='PHOTO'
+        placeholder='사진을 선택하세요!'
         onChange={onChange}
       />
       <PostingInput
-        type="number"
-        id="mealCalories"
-        name="mealCalories"
-        label="CALORIES"
-        placeholder="칼로리를 입력하세요!"
+        type='number'
+        id='mealCalories'
+        name='mealCalories'
+        label='CALORIES'
+        placeholder='칼로리를 입력하세요!'
         onChange={onChange}
       />
       <PostingReviewBox
-        id="mealReview"
-        name="mealReview"
-        label="Review: "
-        placeholder="Leave your comments here!"
+        id='mealReview'
+        name='mealReview'
+        label='Review: '
+        placeholder='Leave your comments here!'
         onChange={onChange}
         onBlur={onBlur}
         hasError={mealData.hasError.review}
@@ -107,45 +107,20 @@ function PostingContainer() {
       <div>
         <p>공개여부</p>
         <PostingInputRadio
-          id="privatePost"
-          name="state"
-          value="private"
-          label="Private"
-          defaultChecked="defaultChecked"
+          id='privatePost'
+          name='state'
+          value='private'
+          label='Private'
+          defaultChecked='defaultChecked'
         />
-        <PostingInputRadio
-          id="publicPost"
-          name="state"
-          value="public"
-          label="Public"
-        />
+        <PostingInputRadio id='publicPost' name='state' value='public' label='Public' />
       </div>
       <div>
         <p>Type</p>
-        <PostingInputRadio
-          id="breakfast"
-          name="mealType"
-          value="breakfast"
-          label="Breakfast"
-        />
-        <PostingInputRadio
-          id="lunch"
-          name="mealType"
-          value="lunch"
-          label="Lunch"
-        />
-        <PostingInputRadio
-          id="dinner"
-          name="mealType"
-          value="dinner"
-          label="Dinner"
-        />
-        <PostingInputRadio
-          id="snack"
-          name="mealType"
-          value="snack"
-          label="Snack"
-        />
+        <PostingInputRadio id='breakfast' name='mealType' value='breakfast' label='Breakfast' />
+        <PostingInputRadio id='lunch' name='mealType' value='lunch' label='Lunch' />
+        <PostingInputRadio id='dinner' name='mealType' value='dinner' label='Dinner' />
+        <PostingInputRadio id='snack' name='mealType' value='snack' label='Snack' />
       </div>
       <PostingButton onSubmit={onSubmit}>등록!</PostingButton>
       <PostingButton>취소</PostingButton>
