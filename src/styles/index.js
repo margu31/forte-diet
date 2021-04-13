f55b144a281b06d60f1afe2bee9bf8a199d69411
lhhyung91 <lhhyung91@gmail.com>
2021-04-13 21:52:44 +0900

    [스타일] 루트 폰트 사이즈 설정

--- src/styles/index.js
+++ src/styles/index.js
@@ -1,4 +1,4 @@
-import { createGlobalStyle } from 'styled-components';
+import { createGlobalStyle } from "styled-components";
 
 const GlobalStyle = createGlobalStyle`
 html, body, div, span, applet, object, iframe,
@@ -55,18 +55,21 @@ table {
 	border-collapse: collapse;
 	border-spacing: 0;
 }
+html {
+  font-size: 10px;
+}
 `;
 
 export const palette = {
-  themePrimary: '#F3B34C',
-  themeSecondary: '#395B9D',
-  themeBright: '#F4F3EF', // 배경 베이지색
-  themeBrightGray: '#B6B6B6',
-  themeDark: '#626262',
-  themeDefault: '#14182C',
-  themeDisable: '#878787',
-  themeHover: '#F5DEB7',
-  themeDefaultWhite: '#FFF'
+  themePrimary: "#F3B34C",
+  themeSecondary: "#395B9D",
+  themeBright: "#F4F3EF", // 배경 베이지색
+  themeBrightGray: "#B6B6B6",
+  themeDark: "#626262",
+  themeDefault: "#14182C",
+  themeDisable: "#878787",
+  themeHover: "#F5DEB7",
+  themeDefaultWhite: "#FFF",
 };
 
 export default GlobalStyle;
