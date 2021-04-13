ea14aed7ba74cc5cec9d10c4000e23abd90dfb62
margu31 <judypark31@gmail.com>
2021-04-13 19:42:07 +0900

    [기능] 공개여부 기능 - radio 버튼에서 toggle로 변경

--- src/containers/PostingContainer/PostingContainer.js
+++ src/containers/PostingContainer/PostingContainer.js
@@ -6,6 +6,7 @@ import PostingForm from "../../components/Posting/PostingForm";
 import PostingInput from "../../components/Posting/PostingInput";
 import PostingInputRadio from "../../components/Posting/PostingInputRadio";
 import PostingReviewBox from "../../components/Posting/PostingReviewBox";
+import PostingToggle from "../../components/Posting/PostingToggle";
 import { addMealAction } from "../../redux/modules/postingMenuList";
 import { reviewValidation } from "../../utils/validation/reviewValidation";
 
@@ -75,26 +76,52 @@ function PostingContainer() {
       <PageTitle>우식이의 오늘의 식단!</PageTitle>
       <PostingForm>
         <PostingInput
-          type="date"
-          id="postingDate"
-          name="postingDate"
-          label="DATE"
-          onChange={onChange}
-          required
-        />
-        <PostingInput
           type="file"
           id="postingFile"
           name="postingFile"
-          label="PHOTO"
-          placeholder="사진을 선택하세요!"
+          label="사진 등록"
           onChange={onChange}
         />
+        <PostingInput
+          type="date"
+          id="postingDate"
+          name="postingDate"
+          label="Date"
+          onChange={onChange}
+          required
+        />
+        <div>
+          <p>Type</p>
+          <PostingInputRadio
+            id="breakfast"
+            name="mealType"
+            value="breakfast"
+            label="아침"
+          />
+          <PostingInputRadio
+            id="lunch"
+            name="mealType"
+            value="lunch"
+            label="점심"
+          />
+          <PostingInputRadio
+            id="dinner"
+            name="mealType"
+            value="dinner"
+            label="저녁"
+          />
+          <PostingInputRadio
+            id="snack"
+            name="mealType"
+            value="snack"
+            label="간식"
+          />
+        </div>
         <PostingInput
           type="number"
           id="mealCalories"
           name="mealCalories"
-          label="CALORIES"
+          label="Calories"
           placeholder="칼로리를 입력하세요!"
           onChange={onChange}
         />
@@ -107,51 +134,24 @@ function PostingContainer() {
           onBlur={onBlur}
           hasError={mealData.hasError.review}
         />
-        <div>
-          <p>공개여부</p>
-          <PostingInputRadio
-            id="privatePost"
-            name="state"
-            value="private"
-            label="Private"
-            defaultChecked="defaultChecked"
-          />
+        {/* <div>
           <PostingInputRadio
             id="publicPost"
             name="state"
             value="public"
             label="Public"
-          />
-        </div>
-        <div>
-          <p>Type</p>
-          <PostingInputRadio
-            id="breakfast"
-            name="mealType"
-            value="breakfast"
-            label="Breakfast"
+            defaultChecked="defaultChecked"
           />
           <PostingInputRadio
-            id="lunch"
-            name="mealType"
-            value="lunch"
-            label="Lunch"
+            id="privatePost"
+            name="state"
+            value="private"
+            label="Private"
           />
-          <PostingInputRadio
-            id="dinner"
-            name="mealType"
-            value="dinner"
-            label="Dinner"
-          />
-          <PostingInputRadio
-            id="snack"
-            name="mealType"
-            value="snack"
-            label="Snack"
-          />
-        </div>
-        <PostingButton onSubmit={onSubmit}>등록!</PostingButton>
+        </div> */}
+        <PostingToggle label1="Public" label2="Private" />
         <PostingButton>취소</PostingButton>
+        <PostingButton onSubmit={onSubmit}>등록!</PostingButton>
       </PostingForm>
     </>
   );
