439b9c660fd355cde7160d4ffdeb6aca84d65638
hyorard-b <reddream08@gmail.com>
2021-04-13 17:57:49 +0900

    [기능] 사이드 내비게이션 추가

---
+++ src/components/Navigation/SideNavigation.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { StyledLi, StyledLink, StyledUl } from './SideNavigation.styled';
+
+export default function SideNavigation() {
+  return (
+    <StyledUl>
+      <StyledLink to='/' exact>
+        <StyledLi>Home</StyledLi>
+      </StyledLink>
+      <StyledLink to='/myPage'>
+        <StyledLi>MyPage</StyledLi>
+      </StyledLink>
+    </StyledUl>
+  );
+}
