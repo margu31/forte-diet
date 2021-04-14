de6a1df80ed25dfd4b42b72d1066e0bf26807be6
margu31 <judypark31@gmail.com>
2021-04-14 21:41:26 +0900

    [스타일] button 스타일링 수정

--- src/components/Button/Button.styled.js
+++ src/components/Button/Button.styled.js
@@ -1,29 +1,24 @@
 import styled, { css } from "styled-components";
 
 export const StyledButton = styled.button`
-  ${({ width, height }) => css`
-    width: ${width}px;
-    height: ${height}px;
-  `}
-  /* border: 0; */
-  /* background-color: transparent; */
+  ${({ $width, $height, $fontSize, $backgroundColor, $color }) =>
+    css`
+      width: ${$width}px;
+      height: ${$height}px;
+      font-size: ${$fontSize}rem;
+      background-color: ${$backgroundColor};
+      color: ${$color};
+      line-height: ${$height}px;
+    `}
 
   font-family: Chungchunsidae;
-  font-size: 1.3rem;
   display: inline-block;
-  background-color: rgba(159, 159, 159, 0.2);
-  width: 100px;
-  height: 30px;
-  line-height: 30px;
   outline: none;
   border: none;
-  color: #14182c;
   border-radius: 5px;
   cursor: pointer;
 
   & + & {
     margin-left: 20px;
-    background-color: rgba(243, 179, 76, 0.2);
-    color: #f26830;
   }
 `;
