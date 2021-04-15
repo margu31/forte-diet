7d970b8153a7885b3ea49ca24de4c7dd192b04ca
hyorard-b <reddream08@gmail.com>
2021-04-15 11:13:23 +0900

    [기능] 페이지 모아서 export

---
+++ src/pages/index.js
@@ -0,0 +1,4 @@
+export { default as Home } from './Home/Home';
+export { default as MyPage } from './MyPage/MyPage';
+export { default as PostingPage } from './PostingPage/PostingPage';
+export { default as SettingPage } from './SettingPage/SettingPage';
