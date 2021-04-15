27d327d6741d0b224f04bbcd4b20212d2c466240
hyorard-b <reddream08@gmail.com>
2021-04-15 19:17:17 +0900

    [스타일] 홈페이지- 웰컴 컨텐츠

--- src/components/Welcome/Welcome.styled.js
+++ src/components/Welcome/Welcome.styled.js
@@ -1,21 +1,26 @@
 import styled, { css } from 'styled-components';
 import { palette } from '../../styles';
-import Button from '../Button/Button';
+import Button from 'components/Button/Button';
+
+/* 래퍼 */
 
 export const StyledWelcomeWrapper = styled.section`
-  min-height: 600px;
+  min-height: 500px;
   background-image: url('/example-bg.jpg');
+  background-repeat: no-repeat;
   display: flex;
   flex-direction: column;
-  padding-left: 180px;
 `;
+
+/* 웰컴 컨텐츠 */
 
 const StyledDiv = styled.div`
   display: flex;
   flex-direction: column;
   align-items: flex-start;
-  padding: 20% 2rem 2rem 2rem;
-  width: 30vw;
+  padding: 7% 2rem 7% 25rem;
+  width: 40vw;
+  min-height: 300px;
 `;
 
 const StyledP = styled.p`
@@ -56,4 +61,81 @@ export const WelcomeContent = () => (
     </StyledP>
     <TutorialBtn>튜토리얼로 이동!</TutorialBtn>
   </StyledDiv>
+);
+
+/* 검색창 */
+
+const SearchSection = styled.section`
+  position: relative;
+  padding-left: 25rem;
+  flex-grow: 1;
+  background: linear-gradient(to right, rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.1));
+`;
+
+const SearchInputForm = styled.form`
+  position: absolute;
+  top: 50%;
+  transform: translate3d(0, -50%, 0);
+`;
+
+const SerachBar = styled.div`
+  position: relative;
+  display: flex;
+  align-items: center;
+  padding: 1rem;
+`;
+
+const SearchLabel = styled.label`
+  position: absolute;
+  top: 50%;
+  transform: translate3d(0, -50%, 0);
+  font-size: 1.2rem;
+  font-weight: 400;
+  left: 3rem;
+`;
+
+const SearchInput = styled.input`
+  width: 200px;
+  height: 50px;
+  border-radius: 50px 0 0 50px;
+  padding-left: 8rem;
+  font-size: 1.5rem;
+  border: none;
+
+  &::placeholder {
+    opacity: 0.5;
+  }
+
+  &:focus {
+    outline: none;
+    ${({ theme }) => css`
+      box-shadow: 0 0 0 4px ${theme.themePrimary};
+    `}
+  }
+  &:focus:not(:focus-visible) {
+    box-shadow: 0;
+  }
+`;
+
+const SearchBtn = styled(Button)`
+  padding: 2rem 4rem;
+  font-size: 1.2rem;
+  background-color: transparent;
+  border-radius: 0 50px 50px 0;
+  color: ${palette.themeDefaultWhite};
+  background-color: ${palette.themePrimary};
+`;
+
+export const Search = () => (
+  <SearchSection>
+    <SearchInputForm legend='식단 검색'>
+      <fieldset>
+        <SerachBar>
+          <SearchLabel>식단 검색: </SearchLabel>
+          <SearchInput placeholder='브로콜리, 카레 ...' />
+          <SearchBtn>Find</SearchBtn>
+        </SerachBar>
+      </fieldset>
+    </SearchInputForm>
+  </SearchSection>
 );
