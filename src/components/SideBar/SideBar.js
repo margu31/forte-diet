41f0c77a135eb7814d4be03c608901ca6a4cc714
hyorard-b <reddream08@gmail.com>
2021-04-15 11:13:11 +0900

    [기능] 사이드바 컨테이너 제거

--- src/components/SideBar/SideBar.js
+++ src/components/SideBar/SideBar.js
@@ -1,9 +1,17 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import HomeLink from '../HomeLink/HomeLink';
 import SideNavigation from '../Navigation/SideNavigation';
 import { StyledSideBar, StyledSideBarWrapper } from './SideBar.styled';
 
-export default function SideBar({ sideBarHeight }) {
+export default function SideBar() {
+  const [sideBarHeight, setSideBarHeight] = useState(0);
+
+  useEffect(() => {
+    window.addEventListener('scroll', () => {
+      setSideBarHeight(window.scrollY);
+    });
+  }, []);
+
   return (
     <StyledSideBarWrapper>
       <StyledSideBar sideBarHeight={sideBarHeight}>
