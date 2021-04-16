b1d649a42759c1e41a8d1b63201785ce9baaef8e
hyorard-b <reddream08@gmail.com>
2021-04-16 19:38:38 +0900

    [스타일] 홈 - 보드 네비게이션 색상 변경

--- src/components/BoardNavigation/BoardNavigation.styled.js
+++ src/components/BoardNavigation/BoardNavigation.styled.js
@@ -2,12 +2,9 @@ import styled, { css } from 'styled-components';
 import { palette } from 'styles';
 import Button from 'components/Button/Button';
 
-/* 래퍼 */
-
-export const StyledBoardWrapper = styled.section``;
-
 export const BoardNav = styled.nav`
   padding: 0 20rem 0;
+  border-top: 0.4rem solid ${palette.themePrimary};
 `;
 
 export const BoardUl = styled.ul`
@@ -18,7 +15,6 @@ export const BoardUl = styled.ul`
 
 export const BoardLi = styled.li`
   position: relative;
-  border-top: 0.4rem solid ${palette.themeBrightYellow};
   min-height: 100%;
   & + & {
     margin-left: 3rem;
@@ -30,7 +26,6 @@ export const BoardBtn = styled(Button)`
     background-color: transparent;
   }
   ${({ $isActive }) =>
-    $isActive &&
     css`
       &:before {
         position: absolute;
@@ -38,7 +33,9 @@ export const BoardBtn = styled(Button)`
         content: '';
         display: block;
         width: 3.6rem;
-        border-bottom: 0.4rem solid ${palette.themePrimary};
+        border-bottom: 0.4rem solid
+          ${$isActive ? palette.themePrimaryThick : palette.themeDefaultWhite};
+        transition: all 0.5s;
       }
       &:after {
         content: '';
