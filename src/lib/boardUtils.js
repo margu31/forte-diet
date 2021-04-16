a1a6cb8f31e5c04753ac4b75f1490177a6340370
hyorard-b <reddream08@gmail.com>
2021-04-16 21:20:41 +0900

    [기능] 게시판 유틸 추가
    상태 크리에이터, 리듀서 크리에이터, 썽크 크리에이터 추가

---
+++ src/lib/boardUtils.js
@@ -0,0 +1,81 @@
+/* 상태 Creator */
+export const boardReducerUtils = {
+  inital: {
+    loading: false,
+    data: null,
+    error: null
+  },
+  loading: prevState => ({
+    loading: true,
+    data: prevState,
+    error: null
+  }),
+  success: data => ({
+    loading: false,
+    data,
+    error: null
+  }),
+  error: error => ({
+    loading: false,
+    data: null,
+    error
+  })
+};
+
+/* Thunk 함수 Creator */
+export const createBoardThunk = (type, api) => {
+  const [SUCCESS, ERROR] = [`${type}_SUCCESS`, `${type}_ERROR`];
+
+  return () => async dispatch => {
+    dispatch({
+      type
+    });
+
+    try {
+      const res = await api(type);
+      dispatch({
+        type: SUCCESS,
+        payload: res
+      });
+    } catch (e) {
+      dispatch({
+        type: ERROR,
+        error: true,
+        payload: e
+      });
+    }
+  };
+};
+
+/* 리듀서 Creator */
+export const getBoardReducer = (type, key) => {
+  const [SUCCESS, ERROR] = [`${type}_SUCCESS`, `${type}_ERROR`];
+
+  return (state, action) => {
+    switch (action.type) {
+      case type:
+        return {
+          ...state,
+          [key]: {
+            ...boardReducerUtils.loading(state[key])
+          }
+        };
+      case SUCCESS:
+        return {
+          ...state,
+          [key]: {
+            ...boardReducerUtils.success(action.payload)
+          }
+        };
+      case ERROR:
+        return {
+          ...state,
+          [key]: {
+            ...boardReducerUtils.error(action.payload)
+          }
+        };
+      default:
+        return state;
+    }
+  };
+};
