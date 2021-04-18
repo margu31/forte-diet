fc534d55c556fb32b3d9ef3e6bf03cda825a818d
margu31 <judypark31@gmail.com>
2021-04-18 21:42:56 +0900

    [스타일] setting 컴포넌트 스타일링

---
+++ src/components/Setting/Setting.styled.js
@@ -0,0 +1,40 @@
+import Input from "components/Input/Input";
+import styled from "styled-components";
+
+export const StyledSettingInput = styled(Input)`
+  label {
+    display: inline-block;
+    font-size: 1.2rem;
+    width: 60px;
+    text-align: right;
+  }
+
+  input {
+    height: 35px;
+    font-size: 1.1rem;
+    margin-left: 15px;
+    margin-bottom: 2rem;
+  }
+`;
+
+export const StyledRadio = styled.div`
+  width: 417px;
+  margin-bottom: 2rem;
+
+  p {
+    display: inline-block;
+    font-size: 1.2rem;
+    text-align: right;
+    width: 60px;
+  }
+
+  label {
+    margin-left: 15px;
+    & + & {
+    }
+  }
+  /* 
+  input {
+    display: inline-block;
+  } */
+`;
