9136192f7389d48929189898e601dd9881da7e47
margu31 <judypark31@gmail.com>
2021-04-19 10:07:51 +0900

    [기능] setting 컴포넌트 및 페이지 구성

--- src/components/Setting/Setting.js
+++ src/components/Setting/Setting.js
@@ -1,31 +1,38 @@
 import Button from "components/Button/Button";
 import Form from "components/Form/Form";
 import InputRadio from "components/Radio/InputRadio";
-import { StyledRadio, StyledSettingInput } from "./Setting.styled";
+import {
+  StyledButtonGroup,
+  StyledRadio,
+  StyledSettingInput,
+  StyledInputWrapper,
+} from "./Setting.styled";
 
 const Setting = () => {
   return (
     <>
       <Form legend="정보 수정">
-        <StyledSettingInput
-          type="text"
-          id="email"
-          label="이메일"
-          readOnly="true"
-        />
-        <StyledSettingInput type="text" id="password" label="패스워드" />
-        <StyledSettingInput type="text" id="nickname" label="닉네임" />
-        <StyledRadio>
-          <p>성별</p>
-          <InputRadio id="male" name="gender" value="male" label="남성" />
-          <InputRadio id="female" name="gender" value="female" label="여성" />
-        </StyledRadio>
-        <StyledSettingInput type="text" id="hieght" label="신장" />
-        <StyledSettingInput type="text" id="weight" label="몸무게" />
-        <div>
+        <StyledInputWrapper>
+          <StyledSettingInput
+            type="text"
+            id="email"
+            label="이메일"
+            readOnly={true}
+          />
+          <StyledSettingInput type="text" id="password" label="패스워드" />
+          <StyledSettingInput type="text" id="nickname" label="닉네임" />
+          <StyledRadio>
+            <p>성별</p>
+            <InputRadio id="male" name="gender" value="male" label="남성" />
+            <InputRadio id="female" name="gender" value="female" label="여성" />
+          </StyledRadio>
+          <StyledSettingInput type="text" id="hieght" label="신장" />
+          <StyledSettingInput type="text" id="weight" label="몸무게" />
+        </StyledInputWrapper>
+        <StyledButtonGroup>
           <Button type="button">취소</Button>
           <Button>수정</Button>
-        </div>
+        </StyledButtonGroup>
       </Form>
     </>
   );
--- src/pages/SettingPage/SettingPage.js
+++ src/pages/SettingPage/SettingPage.js
@@ -1,6 +1,11 @@
 import SettingContainer from "containers/SettingContainer/SettingContainer";
 import React from "react";
+import { StyledSettingPage } from "./SettingPage.styled";
 
 export default function SettingPage() {
-  return <SettingContainer />;
+  return (
+    <StyledSettingPage>
+      <SettingContainer />
+    </StyledSettingPage>
+  );
 }
