691f61ca3b2514af441a1e1537e4342f35fd67d9
margu31 <judypark31@gmail.com>
2021-04-19 22:12:12 +0900

    [스타일] setting 페이지 스타일 변경

--- src/components/Setting/Setting.js
+++ src/components/Setting/Setting.js
@@ -1,41 +1,102 @@
 import Button from "components/Button/Button";
 import Form from "components/Form/Form";
+import Input from "components/Input/Input";
 import InputRadio from "components/Radio/InputRadio";
 import {
   StyledButtonGroup,
   StyledRadio,
   StyledSettingInput,
   StyledInputWrapper,
+  StyledFormWrapper,
 } from "./Setting.styled";
+import { ReactComponent as Edit } from "assets/icons/InputIcons/edit.svg";
 
-const Setting = () => {
-  return (
-    <>
-      <Form legend="정보 수정">
-        <StyledInputWrapper>
-          <StyledSettingInput
-            type="text"
-            id="email"
-            label="이메일"
-            readOnly={true}
-          />
-          <StyledSettingInput type="text" id="password" label="패스워드" />
-          <StyledSettingInput type="text" id="nickname" label="닉네임" />
-          <StyledRadio>
-            <p>성별</p>
-            <InputRadio id="male" name="gender" value="male" label="남성" />
-            <InputRadio id="female" name="gender" value="female" label="여성" />
-          </StyledRadio>
-          <StyledSettingInput type="text" id="hieght" label="신장" />
-          <StyledSettingInput type="text" id="weight" label="몸무게" />
-        </StyledInputWrapper>
-        <StyledButtonGroup>
-          <Button type="button">취소</Button>
-          <Button>수정</Button>
-        </StyledButtonGroup>
-      </Form>
-    </>
-  );
-};
+const Setting = () =>
+  // {
+  // userEmail,
+  // userNickname,
+  // userGender,
+  // userHight,
+  // userWeight,
+  // }
+  {
+    return (
+      <StyledFormWrapper>
+        <Form legend="정보 수정">
+          <StyledInputWrapper>
+            {/* <StyledSettingInput
+              type="text"
+              id="email"
+              label="이메일"
+              // readOnly={true}
+            />
+            <StyledSettingInput type="text" id="password" label="패스워드" />
+            <StyledSettingInput
+              type="text"
+              id="nickname"
+              label="닉네임"
+              // defaultValue={userNickname}
+            />
+            <StyledRadio>
+              <p>성별</p>
+              <InputRadio id="male" name="gender" value="male" label="남성" />
+              <InputRadio
+                id="female"
+                name="gender"
+                value="female"
+                label="여성"
+              />
+            </StyledRadio>
+            <StyledSettingInput type="text" id="hieght" label="신장" />
+            <StyledSettingInput type="text" id="weight" label="몸무게" /> */}
+
+            <StyledSettingInput>
+              <Input
+                type="text"
+                id="email"
+                label="이메일"
+                // readOnly={true}
+              />
+            </StyledSettingInput>
+            <StyledSettingInput>
+              <Input type="text" id="password" label="패스워드" />
+              <Edit />
+            </StyledSettingInput>
+            <StyledSettingInput>
+              <Input
+                type="text"
+                id="nickname"
+                label="닉네임"
+                // defaultValue={userNickname}
+              />
+              <Edit />
+            </StyledSettingInput>
+            <StyledRadio>
+              <p>성별</p>
+              <InputRadio id="male" name="gender" value="male" label="남성" />
+              <InputRadio
+                id="female"
+                name="gender"
+                value="female"
+                label="여성"
+              />
+            </StyledRadio>
+            <StyledSettingInput>
+              <Input type="text" id="hieght" label="신장" />
+              <Edit />
+            </StyledSettingInput>
+            <StyledSettingInput>
+              <Input type="text" id="weight" label="몸무게" />
+              <Edit />
+            </StyledSettingInput>
+          </StyledInputWrapper>
+          <StyledButtonGroup>
+            <Button type="button">취소</Button>
+            <Button>수정</Button>
+          </StyledButtonGroup>
+        </Form>
+      </StyledFormWrapper>
+    );
+  };
 
 export default Setting;
--- src/components/Setting/Setting.styled.js
+++ src/components/Setting/Setting.styled.js
@@ -1,11 +1,46 @@
-import Input from "components/Input/Input";
 import styled from "styled-components";
+import { palette } from "styles";
 
-export const StyledInputWrapper = styled.div`
-  margin-top: 4rem;
+export const StyledFormWrapper = styled.div`
+  margin-top: 5rem;
+  padding: 0 0 4rem 1.8rem;
+  border-radius: 5px;
+  background-color: ${palette.themeBrightYellow};
+  /* box-shadow: 0 0 10px 5px ${palette.themePrimary}; */
+  box-shadow: 0 0 5px 5px ${palette.themeTertiary};
 `;
 
-export const StyledSettingInput = styled(Input)`
+export const StyledInputWrapper = styled.div`
+  margin-top: 4.5rem;
+`;
+
+// export const StyledSettingInput = styled(Input)`
+//   label {
+//     display: inline-block;
+//     font-size: 1.2rem;
+//     width: 60px;
+//     text-align: right;
+//   }
+
+//   input {
+//     width: 30rem;
+//     height: 35px;
+//     font-size: 1.1rem;
+//     margin-left: 15px;
+//     margin-bottom: 2rem;
+
+//     // 스타일링 바꿔보는중
+//     border: none;
+//     border-radius: 0;
+//     border-bottom: 2px solid ${palette.themeTertiary};
+//     background-color: inherit;
+//   }
+// `;
+
+export const StyledSettingInput = styled.div`
+  position: relative;
+  /* padding-left: 1rem; */
+
   label {
     display: inline-block;
     font-size: 1.2rem;
@@ -14,10 +49,35 @@ export const StyledSettingInput = styled(Input)`
   }
 
   input {
+    box-sizing: border-box;
+    width: 30rem;
     height: 35px;
     font-size: 1.1rem;
-    margin-left: 15px;
+    margin-left: 2rem;
     margin-bottom: 2rem;
+    padding-right: 35px;
+    border-radius: 0;
+
+    border: 0;
+    border-bottom: 1px solid ${palette.themePrimary};
+
+    &:focus {
+      border: 2px solid;
+      /* border-image: linear-gradient(to left, #61c69d 0%, #2d72bc 100%); */
+      border-image: linear-gradient(to left, #f12711 0%, #f5af19 100%);
+      /* border-image: linear-gradient(to left, #1e9600, #fff200, #ff0000); */
+      border-image-slice: 1;
+
+      /* transition: border linear 0.3s; */
+    }
+  }
+
+  svg {
+    position: absolute;
+    top: 7px;
+    right: 45px;
+    cursor: pointer;
+    /* opacity: 0; */
   }
 `;
 
@@ -30,17 +90,20 @@ export const StyledRadio = styled.div`
     font-size: 1.2rem;
     text-align: right;
     width: 60px;
+    margin-right: 2rem;
   }
 
   label {
-    margin-left: 15px;
-    & + & {
+    box-sizing: border-box;
+    width: 151px;
+    height: 35px;
+    line-height: 35px;
+
+    &:hover {
+      /* border: 2px solid #f5af19; */
+      background: linear-gradient(to bottom, #fdc830, #f37335);
     }
   }
-  /* 
-  input {
-    display: inline-block;
-  } */
 `;
 
 export const StyledButtonGroup = styled.div`
