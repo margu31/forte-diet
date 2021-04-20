5d4d157e0803f06eb27e8bb584754cda52684d0a
jjhstoday <jjh.s.today@gmail.com>
2021-04-20 10:09:21 +0900

    [pages] MyPage에 UserBar 추가

--- src/pages/MyPage/MyPage.js
+++ src/pages/MyPage/MyPage.js
@@ -3,10 +3,12 @@ import { StyledMyPageWrapper } from './MyPage.styled';
 import MenuListContainer from 'containers/MenuListContainer/MenuListContainer';
 import HealthBarContainer from 'containers/HealthBarContainer/HealthBarContainer';
 import CalendarContainer from 'containers/CalendarContainer/CalendarContainer';
+import UserBar from 'containers/UserBar/UserBar';
 
 export default function MyPage({ history }) {
   return (
     <StyledMyPageWrapper>
+      <UserBar />
       <HealthBarContainer />
       <MenuListContainer history={history} />
       <CalendarContainer />
