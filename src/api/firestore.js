8d876a59ebbda7502e97d01e5d626f02bcde6563
hyorard-b <reddream08@gmail.com>
2021-04-20 19:09:19 +0900

    [기능] diet 추가 api 추가

--- src/api/firestore.js
+++ src/api/firestore.js
@@ -2,13 +2,11 @@ import { firestore } from './auth';
 import firebase from 'firebase';
 
 const users = firestore.collection('users');
+const diets = firestore.collection('diets');
 
 /* 마이 페이지 */
 
-export const handleGetDietLists = (
-  { uid },
-  updateDietAction
-) => async dispatch => {
+export const handleGetDietLists = ({ uid }, updateDietAction) => async dispatch => {
   try {
     const snapshot = await users.where('id', '==', uid).get();
 
@@ -60,12 +58,7 @@ export const removeDailyReview = ({ uid }, date) => async () => {
   }
 };
 
-export const addWaterDose = (
-  { uid },
-  date,
-  currentDose,
-  additionalDose
-) => async () => {
+export const addWaterDose = ({ uid }, date, currentDose, additionalDose) => async () => {
   try {
     const user = await users.doc(uid);
     user.set(
@@ -109,7 +102,21 @@ export const removeMeal = ({ uid }, dietList, date, mealId) => async () => {
 
 /* 포스팅 페이지 */
 
-// export const newMeal = async
+export const addNewDiet = async mealdata => {
+  try {
+    const newDietRef = await diets.add({
+      dailyReview: null,
+      createdAt: new Date(),
+      calorie: mealdata.calorie,
+      like: 0,
+      meals: [{ ...mealdata }]
+    });
+
+    return newDietRef.id;
+  } catch (e) {
+    throw new Error(e.message);
+  }
+};
 
 export const PostMeal = async ({ uid }, mealdata) => {
   try {
