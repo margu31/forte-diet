c4ba7d49ce3fb90897a1e7875f34c0d67fb8fb59
hyorard-b <reddream08@gmail.com>
2021-04-22 15:08:20 +0900

    [스타일] 사이드 네비게이션 변경

--- src/components/Navigation/Navigation.styled.js
+++ src/components/Navigation/Navigation.styled.js
@@ -1,28 +1,61 @@
 import { NavLink } from 'react-router-dom';
-import styled, { css } from 'styled-components';
+import styled from 'styled-components';
+import { AiOutlineUser } from 'react-icons/ai';
+import { BsHouse, BsPen, BsGear } from 'react-icons/bs';
+import { palette } from '../../styles';
 
 export const StyledUl = styled.ul`
   display: inline-flex;
   flex-direction: column;
   align-items: flex-end;
+  width: 100%;
 `;
 
 export const StyledLi = styled.li`
-  margin-left: 5rem;
+  width: 100%;
 `;
 
 export const StyledNavLink = styled(NavLink)`
-  display: inline-block;
-  width: 6rem;
-  padding: 22px 150px 22px 22px;
-  font-size: 1.6rem;
-  border-radius: 20px 0 0 20px;
-  ${({ theme }) => css`
-    color: ${theme.themeDefaultWhite};
-    &.active {
-      color: ${theme.themePrimary};
-      background-color: ${theme.themeBright};
-      box-shadow: -5px 5px rgba(0, 0, 0, 0.25);
-    }
-  `}
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 2rem 0;
+  width: 100%;
+  font-size: 1.4rem;
+  color: ${palette.themeDefault};
+
+  transition: all 0.2s;
+
+  &:hover {
+    transform: scale(1.2, 1.2);
+  }
+
+  &.active {
+    background-color: ${palette.themeBright};
+    transform: none;
+  }
+`;
+
+export const IconContainer = styled.div`
+  margin-right: 0.5rem;
+`;
+
+export const HomeIcon = styled(BsHouse)`
+  width: 2rem;
+  height: 2rem;
+`;
+
+export const MyPageIcon = styled(AiOutlineUser)`
+  width: 2rem;
+  height: 2rem;
+`;
+
+export const PostingIcon = styled(BsPen)`
+  width: 2rem;
+  height: 2rem;
+`;
+
+export const SettingIcon = styled(BsGear)`
+  width: 2rem;
+  height: 2rem;
 `;
