206566eaf6ed46ac0f49c485a034b2caa2895bfa
hyorard-b <reddream08@gmail.com>
2021-04-22 17:53:35 +0900

    [스타일] 사이드 내브 링크에 아이콘 추가

--- src/components/Navigation/Navigation.styled.js
+++ src/components/Navigation/Navigation.styled.js
@@ -1,8 +1,9 @@
 import { NavLink } from 'react-router-dom';
 import styled from 'styled-components';
-import { AiOutlineUser } from 'react-icons/ai';
-import { BsHouse, BsPen, BsGear } from 'react-icons/bs';
+import { BsHouse, BsGear } from 'react-icons/bs';
 import { fontSizeKit, fontWeightKit, palette } from '../../styles';
+import { IoPizzaOutline } from 'react-icons/io5';
+import { RiEditBoxLine } from 'react-icons/ri';
 
 export const StyledUl = styled.ul`
   display: inline-flex;
@@ -12,28 +13,39 @@ export const StyledUl = styled.ul`
 `;
 
 export const StyledLi = styled.li`
+  padding-left: 2rem;
   width: 100%;
-  text-align: center;
+  text-align: start;
+
+  & + & {
+    margin-top: 1rem;
+  }
 `;
 
 export const StyledNavLink = styled(NavLink)`
   display: inline-flex;
   align-items: center;
-  justify-content: center;
-  padding: 2rem 8rem 2rem 2rem;
+  justify-content: flex-start;
+  box-sizing: border-box;
+  width: 90%;
+  padding: 2rem 7rem 2rem 2rem;
   font-size: ${fontSizeKit.medium};
   font-weight: ${fontWeightKit.normal};
   color: ${palette.themeDefaultWhite};
+  border-radius: 10px;
 
   &.active {
-    background-color: rgba(0, 0, 0, 0.2);
-    border-radius: 10px;
+    /* background-color: rgba(0, 0, 0, 0.2); */
+    background-color: ${palette.themeBright};
+    color: ${palette.themePrimaryThick};
+    font-weight: ${fontWeightKit.bold};
     transform: none;
+    box-shadow: -4px 4px 0 rgba(0, 0, 0, 0.1);
   }
 `;
 
 export const IconContainer = styled.div`
-  margin-right: 0.5rem;
+  margin-right: 0.7rem;
 `;
 
 export const HomeIcon = styled(BsHouse)`
@@ -41,17 +53,17 @@ export const HomeIcon = styled(BsHouse)`
   height: 2rem;
 `;
 
-export const MyPageIcon = styled(AiOutlineUser)`
-  width: 2rem;
-  height: 2rem;
-`;
-
-export const PostingIcon = styled(BsPen)`
+export const PostingIcon = styled(RiEditBoxLine)`
   width: 2rem;
   height: 2rem;
 `;
 
 export const SettingIcon = styled(BsGear)`
+  width: 2rem;
+  height: 2rem;
+`;
+
+export const MealIcon = styled(IoPizzaOutline)`
   width: 2rem;
   height: 2rem;
 `;
