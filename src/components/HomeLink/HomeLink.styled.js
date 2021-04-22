9dff547d4bc8ba96fbad5223b6a2cb23ad837184
hyorard-b <reddream08@gmail.com>
2021-04-22 15:08:03 +0900

    [스타일] 홈 로고 변경
    사진 -> 텍스트

--- src/components/HomeLink/HomeLink.styled.js
+++ src/components/HomeLink/HomeLink.styled.js
@@ -1,6 +1,16 @@
-import { ReactComponent as logo } from 'assets/logos/logo.svg';
 import styled from 'styled-components';
+import { palette } from '../../styles';
 
-export const StyledLogo = styled(logo)`
-  margin-bottom: 74px;
+export const StyledLogo = styled.div`
+  padding: 5rem;
+  font-size: 2rem;
+  margin: 3rem 0;
+  color: ${palette.themeDefaultWhite};
+  border-radius: 10px;
+
+  transition: all 0.2s;
+
+  &:hover {
+    transform: scale(1.2, 1.2);
+  }
 `;
