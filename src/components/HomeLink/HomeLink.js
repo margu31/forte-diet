848ba7aa15cd35a971137e2c4231469c6e137677
hyorard-b <reddream08@gmail.com>
2021-04-22 17:53:20 +0900

    [스타일] 홈 로고 변경

--- src/components/HomeLink/HomeLink.js
+++ src/components/HomeLink/HomeLink.js
@@ -1,26 +1,12 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { StyledLogo, Usik, UsikContainer } from './HomeLink.styled';
+import { StyledLogo } from './HomeLink.styled';
 
 export default function HomeLink() {
   return (
     <h1>
       <Link to='/'>
-        <StyledLogo>
-          <UsikContainer
-            initial={{ x: 0 }}
-            whileHover={{
-              x: [0, -100, 0],
-              transition: {
-                duration: 1,
-                type: 'spring'
-              }
-            }}
-          >
-            <Usik />
-          </UsikContainer>
-          우연히, 식단
-        </StyledLogo>
+        <StyledLogo>우연히, 식단</StyledLogo>
       </Link>
     </h1>
   );
--- src/components/HomeLink/HomeLink.styled.js
+++ src/components/HomeLink/HomeLink.styled.js
@@ -1,27 +1,13 @@
 import styled from 'styled-components';
-import { fontSizeKit, fontWeightKit, palette } from 'styles';
-import { ReactComponent as UglyFox } from 'assets/logos/logo1.svg';
-import { motion } from 'framer-motion';
+import { fontWeightKit, palette } from 'styles';
 
 export const StyledLogo = styled.div`
-  font-family: InkLipquid;
+  font-family: 'InkLipquid';
   position: relative;
   padding: 5rem 0;
-  font-size: ${fontSizeKit.xLarge};
-  font-weight: ${fontWeightKit.extraBold};
+  font-size: 3.8rem;
+  font-weight: ${fontWeightKit.normal};
   margin: 3rem 0;
   color: ${palette.themeDefaultWhite};
   border-radius: 10px;
-  text-shadow: 2px 2px 4px ${palette.themeDark};
-`;
-
-export const UsikContainer = styled(motion.div)`
-  position: absolute;
-  top: 0;
-  right: 0;
-`;
-
-export const Usik = styled(UglyFox)`
-  width: 50px;
-  height: 50px;
 `;
