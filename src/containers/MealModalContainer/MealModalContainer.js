9e77fec7e0786ae5dba5e11957c48adcbfcd3464
lhhyung91 <lhhyung91@gmail.com>
2021-04-24 21:24:15 +0900

    [기능] mealModal 기능 구현, 캐러셀 진행 중

--- src/containers/MealModalContainer/MealModalContainer.js
+++ src/containers/MealModalContainer/MealModalContainer.js
@@ -5,64 +5,26 @@ import { useSelector } from "react-redux";
 import { StyledMealModalContainer } from "./MealModalContainer.styled";
 
 export default function MealModalContainer({ onMealModal, mealListData }) {
-  const auth = useSelector((state) => state.auth);
   const menuList = useSelector((state) => state.menuList);
 
-  const [user, setUser] = useState(auth);
-  const { id, photo, review } = mealListData;
-  const [dailyId, setDailyId] = useState(id);
+  const { date } = mealListData;
 
-  const mealsArrayList = Object.entries(menuList)[0][1].meals;
   // meals 배열
-  const [dayilyMeal, setDayilyMeal] = useState(mealsArrayList);
+  const mealsArrayList = Object.entries(menuList)
+    .sort((a, b) => b[0].slice(0, 6) - a[0].slice(0, 6))
+    .map((data) => data[1])
+    .filter((data) => data.date === date)[0].meals;
 
-  console.log(dayilyMeal);
-  console.log(dailyId);
-  // const { id, photo, review } = dayilyMeal;
-  const dailyMealChange = (dailyId) => {
-    dayilyMeal.filter((data) => data.id === dailyId);
-    console.log(dayilyMeal);
-  };
-  const mealNumberId = parseInt(dailyId, 10);
-  // console.log(typeof parseInt(dailyId, 10));
-  // console.log(typeof dailyId);
-  // console.log(mealNumberId);
-  // console.log(dayilyMeal);
+  const prevMeal = (e) => {};
 
-  // const check = dayilyMeal.map((id) => id === dailyId);
-  // const checkcheck = dayilyMeal.map((id) => +id);
-  // console.log(check);
-  // console.log(checkcheck);
-  // useEffect(() => {
-  //   if (auth.isAuthed === true) {
-  //     const menuListData = Object.entries(menuList).map((data) => data[1])[0]
-  //       .meals;
-  //     console.log(menuList.meals);
-  //   }
-  // }, []);
-  // const menuListData = Object.entries(menuList).map((data) => data[1])[0].meals;
+  const nextMeal = (e) => {};
 
-  const prevMeal = (e) => {
-    if (dailyId === 0) return;
-    if (dailyId === mealNumberId) {
-    }
-    setDailyId(mealNumberId - 1);
-    // dailyMealChange(dailyId);
-  };
-
-  const nextMeal = (e) => {
-    setDailyId(mealNumberId + 1);
-  };
-
-  // const dialyMeal =
-  //   mealsArrayList.map((id, review, photo) => )
-  console.log(mealsArrayList);
   return (
     <StyledMealModalContainer>
       <Modal>
         {mealsArrayList.map(({ id, review, photo }) => (
           <MealDialog
-            dailyId={dailyId}
+            key={id}
             id={id}
             photo={photo}
             review={review}
@@ -72,16 +34,6 @@ export default function MealModalContainer({ onMealModal, mealListData }) {
             mealListData={mealListData}
           ></MealDialog>
         ))}
-        {/* <MealDialog
-          dailyId={dailyId}
-          id={id}
-          photo={photo}
-          review={review}
-          onMealModal={onMealModal}
-          prevMeal={prevMeal}
-          nextMeal={nextMeal}
-          mealListData={mealListData}
-        ></MealDialog> */}
       </Modal>
     </StyledMealModalContainer>
   );
