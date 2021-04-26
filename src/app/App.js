eafc3100a5123517b11a1a0f71f44a71cf29b57c
margu31 <judypark31@gmail.com>
2021-04-26 23:05:31 +0900

    [기능] 포스트 수정페이지 추가

--- src/app/App.js
+++ src/app/App.js
@@ -1,10 +1,11 @@
-import GlobalStyle, { palette } from 'styles';
-import { Switch, Route } from 'react-router-dom';
-import { ThemeProvider } from 'styled-components';
-import './App.css';
-import { StyledApp } from './App.styled';
-import { Home, MyPage, PostingPage, SettingPage } from 'pages';
-import SideBar from 'components/SideBar/SideBar';
+import GlobalStyle, { palette } from "styles";
+import { Switch, Route } from "react-router-dom";
+import { ThemeProvider } from "styled-components";
+import "./App.css";
+import { StyledApp } from "./App.styled";
+import { Home, MyPage, PostingPage, SettingPage } from "pages";
+import SideBar from "components/SideBar/SideBar";
+import PostEditPage from "pages/PostEdit/PostEditPage";
 
 function App() {
   return (
@@ -13,10 +14,11 @@ function App() {
       <ThemeProvider theme={palette}>
         <SideBar />
         <Switch>
-          <Route path='/' exact component={Home} />
-          <Route path='/myPage' component={MyPage} />
-          <Route path='/posting' component={PostingPage} />
-          <Route path='/setting' component={SettingPage} />
+          <Route path="/" exact component={Home} />
+          <Route path="/myPage" component={MyPage} />
+          <Route path="/posting" component={PostingPage} />
+          <Route path="/setting" component={SettingPage} />
+          <Route path="/postedit" component={PostEditPage} />
           {/* Redirect 폴백 페이지 추가하여야 함 */}
         </Switch>
       </ThemeProvider>
