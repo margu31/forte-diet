7f416df19c754b6d1a3c0b2100eaccdb86228fc7
hyorard-b <reddream08@gmail.com>
2021-04-27 04:01:47 +0900

    [기능] 이미지 아마존 S3 업로드 api 연동

--- src/components/DataGroup/DataGroup.js
+++ src/components/DataGroup/DataGroup.js
@@ -1,7 +1,7 @@
-import DragDrop from "components/DragDrop/DragDrop";
-import Input from "components/Input/Input";
-import RadioGroup from "components/RadioGroup/RadioGroup";
-import StyledDataGroup from "./DataGroup.styled";
+import DragDrop from 'components/DragDrop/DragDrop';
+import Input from 'components/Input/Input';
+import RadioGroup from 'components/RadioGroup/RadioGroup';
+import StyledDataGroup from './DataGroup.styled';
 
 const DataGroup = ({
   onChange,
@@ -20,6 +20,7 @@ const DataGroup = ({
   isDragging,
   loadedFile,
   imgRef,
+  fileRef
 }) => {
   // const today = new Date();
   // const year = today.getFullYear();
@@ -50,39 +51,40 @@ const DataGroup = ({
         isDragging={isDragging}
         loadedFile={loadedFile}
         imgRef={imgRef}
+        fileRef={fileRef}
       />
-      <div className="collection">
+      <div className='collection'>
         <Input
-          type="date"
-          id="postingDate"
-          name="date"
-          label="Date"
+          type='date'
+          id='postingDate'
+          name='date'
+          label='Date'
           onChange={onChange}
           max={maxDate}
           defaultValue={maxDate.toString()}
         />
-        <RadioGroup groupTitle="Type" onChange={onChange} />
+        <RadioGroup groupTitle='Type' onChange={onChange} />
         <Input
-          type="number"
-          id="mealCalories"
-          name="calories"
-          label="Calories"
-          placeholder="칼로리를 입력하세요!"
+          type='number'
+          id='mealCalories'
+          name='calories'
+          label='Calories'
+          placeholder='칼로리를 입력하세요!'
           onChange={onChange}
           onKeyPress={onKeyPress}
-          min="0"
+          min='0'
         />
         <Input
-          type="text"
-          id="mealTitle"
-          placeholder="오늘의 메뉴를 입력하세요! (10자 이내)"
-          name="title"
-          label="Menu"
+          type='text'
+          id='mealTitle'
+          placeholder='오늘의 메뉴를 입력하세요! (10자 이내)'
+          name='title'
+          label='Menu'
           onChange={onChange}
           // onBlur={onBlur}
           onKeyUp={onKeyUp}
           errorMessage={errorMessage}
-          maxLength="10"
+          maxLength='10'
         />
       </div>
     </StyledDataGroup>
