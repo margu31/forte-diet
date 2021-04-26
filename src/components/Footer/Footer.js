f60737a0397ac3195a728dcec92b2650d9fa5213
hyorard-b <reddream08@gmail.com>
2021-04-26 22:54:13 +0900

    [기능] 푸터 컴포넌트 추가

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,99 @@
+import React from 'react';
+import {
+  FooterWrapper,
+  FooterColumn,
+  StyledFox,
+  FooterContentHeader,
+  FooterContent,
+  FooterLink,
+  GithubIcon,
+  FacebookIcon,
+  TwitterIcon,
+  FooterInfoWrapper,
+  Rights,
+  RightWrapper,
+  RightContent,
+  ContactHeader,
+  ContactColumn
+} from './Footer.styled';
+
+export default function Footer() {
+  return (
+    <FooterWrapper>
+      <FooterInfoWrapper>
+        <FooterColumn>
+          <FooterContentHeader>
+            <StyledFox />
+            Forte Diet
+          </FooterContentHeader>
+          <FooterContent>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+            incididunt ut labore et dolore magna aliqua.
+          </FooterContent>
+        </FooterColumn>
+        <ContactColumn>
+          <ContactHeader>CONTACT US</ContactHeader>
+          <FooterContent>
+            김효성
+            <FooterLink href='https://github.com/hyorard-b'>
+              <GithubIcon />
+            </FooterLink>
+            <FooterLink href='https://github.com/hyorard-b'>
+              <FacebookIcon />
+            </FooterLink>
+            <FooterLink href='https://github.com/hyorard-b'>
+              <TwitterIcon />
+            </FooterLink>
+          </FooterContent>
+          <FooterContent>
+            박혜준
+            <FooterLink href='https://github.com/margu31'>
+              <GithubIcon />
+            </FooterLink>
+            <FooterLink href='https://github.com/margu31'>
+              <FacebookIcon />
+            </FooterLink>
+            <FooterLink href='https://github.com/margu31'>
+              <TwitterIcon />
+            </FooterLink>
+          </FooterContent>
+          <FooterContent>
+            이효형
+            <FooterLink href='https://github.com/lhhyung91'>
+              <GithubIcon />
+            </FooterLink>
+            <FooterLink href='https://github.com/lhhyung91'>
+              <FacebookIcon />
+            </FooterLink>
+            <FooterLink href='https://github.com/lhhyung91'>
+              <TwitterIcon />
+            </FooterLink>
+          </FooterContent>
+          <FooterContent>
+            장지혜
+            <FooterLink href='https://github.com/jjhstoday'>
+              <GithubIcon />
+            </FooterLink>
+            <FooterLink href='https://github.com/jjhstoday'>
+              <FacebookIcon />
+            </FooterLink>
+            <FooterLink href='https://github.com/jjhstoday'>
+              <TwitterIcon />
+            </FooterLink>
+          </FooterContent>
+        </ContactColumn>
+        <FooterColumn>
+          <FooterContentHeader>FOLLOW US</FooterContentHeader>
+          <FooterContent>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+            incididunt ut labore et dolore magna aliqua.
+          </FooterContent>
+        </FooterColumn>
+      </FooterInfoWrapper>
+      <RightWrapper>
+        <RightContent>302-2, Gangnam-daero, Gangnam-gu, Seoul, Republic of Korea</RightContent>
+        <RightContent>Powered by React, Firebase etc.</RightContent>
+      </RightWrapper>
+    </FooterWrapper>
+  );
+}
---
+++ src/components/Footer/Footer.styled.js
@@ -0,0 +1,98 @@
+import styled, { css } from 'styled-components';
+import { ReactComponent as Fox } from 'assets/logos/logo1.svg';
+import { fontSizeKit, palette } from '../../styles';
+import { GrGithub } from 'react-icons/gr';
+import { CgTwitter } from 'react-icons/cg';
+import { FaFacebook } from 'react-icons/fa';
+
+export const FooterWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 5rem 20rem;
+  background-color: ${palette.themeFooter};
+  color: ${palette.themeDefaultWhite};
+`;
+
+export const FooterInfoWrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: flex-start;
+  padding-bottom: 4rem;
+`;
+
+export const FooterColumn = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: flex-start;
+  width: 30%;
+  flex-wrap: wrap;
+`;
+
+export const FooterContentHeader = styled.div`
+  display: flex;
+  align-items: center;
+  font-size: ${fontSizeKit.medium};
+  margin-bottom: 3rem;
+`;
+
+export const ContactColumn = styled(FooterColumn)`
+  width: 20%;
+`;
+
+export const ContactHeader = styled(FooterContentHeader)`
+  margin-bottom: 2rem;
+`;
+
+export const FooterContent = styled.p`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: ${fontSizeKit.small};
+  line-height: ${fontSizeKit.large};
+`;
+
+export const FooterLink = styled.a``;
+
+const iconStyle = () => css`
+  width: 25px;
+  height: 25px;
+  color: ${palette.themeDefaultWhite};
+`;
+
+export const GithubIcon = styled(GrGithub)`
+  ${iconStyle()};
+  margin-left: 1rem;
+`;
+
+export const FacebookIcon = styled(FaFacebook)`
+  ${iconStyle()};
+  margin-left: 3rem;
+`;
+
+export const TwitterIcon = styled(CgTwitter)`
+  ${iconStyle()};
+  color: ${palette.themeFooter};
+  background-color: ${palette.themeDefaultWhite};
+  border-radius: 50%;
+  margin-left: 3rem;
+`;
+
+export const StyledFox = styled(Fox)`
+  width: 25px;
+  height: 25px;
+  cursor: pointer;
+  border-radius: 50%;
+  margin-right: 1rem;
+`;
+
+export const RightWrapper = styled.div`
+  border-top: 1px solid ${palette.themeBrightGray};
+  display: flex;
+  padding-top: 4rem;
+  justify-content: space-between;
+`;
+
+export const RightContent = styled.p`
+  color: ${palette.themeBrightGray};
+`;
