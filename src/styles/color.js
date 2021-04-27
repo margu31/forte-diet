8834feb66dd2d76031a757ab420d4f78ae12663f
hyorard-b <reddream08@gmail.com>
2021-04-27 12:10:12 +0900

    [빌드] 기본 스타일 폴더 구조 변경

---
+++ src/styles/color.js
@@ -0,0 +1,17 @@
+export const palette = {
+  themePrimary: '#f3b34c',
+  themeSecondary: '#395B9D',
+  themePrimaryThick: '#F26830',
+  themeTertiary: '#a2bf63',
+  themeQuaternary: '#ed5e5e',
+  themeBright: '#F4F3EF', // 배경 베이지색
+  themeBrightYellow: '#FEFDF8', // input, textarea 배경색
+  themeBrightGray: '#B6B6B6',
+  themeDark: '#626262',
+  themeDefault: '#14182C',
+  themeDisable: '#878787',
+  themeHover: '#F5DEB7',
+  themeDefaultWhite: '#FFF',
+  themeFont: '#4a4a4a',
+  themeFooter: '#3c3c3c'
+};
