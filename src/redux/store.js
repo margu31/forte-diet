65a09f912af4cf4bda28842e579c8d2d9f7a5fdb
hyorard-b <reddream08@gmail.com>
2021-04-27 20:12:19 +0900

    [디버그] 로거 미들웨어를 마지막 미들웨어로 변경

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,15 +1,12 @@
-import { createStore, applyMiddleware } from "redux";
-import { Provider } from "react-redux";
-import { composeWithDevTools } from "redux-devtools-extension";
-import rootreducer from "./modules";
-import logger from "redux-logger";
-import thunk from "redux-thunk";
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import rootreducer from './modules';
+import logger from 'redux-logger';
+import thunk from 'redux-thunk';
 
-const middlewares = [logger, thunk];
-const store = createStore(
-  rootreducer,
-  composeWithDevTools(applyMiddleware(...middlewares))
-);
+const middlewares = [thunk, logger];
+const store = createStore(rootreducer, composeWithDevTools(applyMiddleware(...middlewares)));
 
 export function StoreProvider(props) {
   return <Provider store={store} {...props} />;
