e0c7f7d5b89dd7a520590ac32705882a549307f8
hyorard-b <reddream08@gmail.com>
2021-04-27 20:00:53 +0900

    [디버그] 미사용 태그, 모듈 제거

--- src/components/Welcome/Welcome.styled.js
+++ src/components/Welcome/Welcome.styled.js
@@ -1,4 +1,4 @@
-import styled, { css } from 'styled-components';
+import styled from 'styled-components';
 import { fontSizeKit, palette } from '../../styles';
 import Button from 'components/Button/Button';
 
