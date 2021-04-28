54f15d91d07598b2bbfce0a96420c489df098fc3
margu31 <judypark31@gmail.com>
2021-04-29 00:34:49 +0900

    [리팩토링] Toggle 컴포넌트 aria-label 추가(웹 접근)

--- src/components/Toggle/Toggle.js
+++ src/components/Toggle/Toggle.js
@@ -24,8 +24,15 @@ const Toggle = ({
         onChange={onChange}
         checked={isEditing && defaultIsPublic === "private"}
       />
-      <label tabIndex="0" htmlFor={id} onKeyDown={onKeyDown}>
+      <label
+        tabIndex="0"
+        htmlFor={id}
+        onKeyDown={onKeyDown}
+        aria-label="공개 여부를 선택하세요. 미 선택시 전체 공개로 포스팅됩니다."
+      >
+        {/* <span tabIndex="0" role="checkbox" aria-checked="false"> */}
         <span>{label1}</span>
+        {/* <span tabIndex="0" role="checkbox" aria-checked="true"> */}
         <span>{label2}</span>
         <span></span>
       </label>
